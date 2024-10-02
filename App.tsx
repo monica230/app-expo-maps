c4888fe6c161c38a1fefd3af0b5a05f03228b1ce
monica230 <monicamaltacouto@gmail.com>
2024-10-02 20:12:26 -0300

    commit inicial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, StatusBar, Text } from 'react-native';
+import MapView from 'react-native-maps'
+import * as Location from 'expo-location'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor='#f3c715' />
+      <MapView style={styles.map} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
+  map: {
+    width: '100%',
+    height: '100%'
+  }
 });
